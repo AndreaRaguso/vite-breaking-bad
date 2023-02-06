9044f7e954b5f3ae6449785604ea7898a3a970fe
Andrea Raguso <info@andrearaguso.com>
2023-02-06 18:31:23 +0100

    esercizio finito

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <script>
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+
 
 export default {
   name: "App",
   components:{
     AppHeader,
     AppMain
-  }
+  },
+  data() {
+        return {
+            card: []
+        }
+  },  
+  created() {
+        axios
+            .get('https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Alien')
+            .then((response) => {
+                this.card = response.data.data.slice(0,20)
+                
+            });
+    }
 }
 
 </script>
@@ -15,7 +30,7 @@ export default {
 <template>
 
   <AppHeader/>
-  <AppMain/>
+  <AppMain :cardList="card"/>
 
 </template>
 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,55 @@
 <script>
 export default {
     name: 'AppMain',
+    props:{
+        cardList:{
+            type: Array,
+            default: []
+        }
+    }
+    
 }
 </script>
 
 <template>
     
-    <div></div>
+    <div class="container ">
+
+        <div class="col-2 my-3">
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Seleziona..</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
+
+        <div class="bg-white">
+
+            <div class="container py-5">
+                <div class="bg-dark py-2">
+                    <strong class="text-white mx-3">Found {{ cardList.length }} cards</strong> 
+                </div>
+                <div >
+                    <div class="row">
+                        <div class="col-2 mb-4" v-for="card in cardList">
+                            <div>
+                                <img :src= "card.card_images[0].image_url" alt="" class=" img-fluid">
+                            </div>
+                            <div class="info p-3">
+                                <h3 class="text-white"> {{ card.name }}</h3>
+                                <h4 class="text-center">{{card.archetype}}</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 
 </template>
 
 <style lang="scss" scoped>
+@import '../styles/partials/AppMain.scss';
 </style>
